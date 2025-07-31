4fdce6ebb5be20b1f775a22b69dcffd14f44ca90
Raginiravi772020 <raginiravipandit@gmail.com>
2025-07-31 15:31:04 +0530

    Add files via upload

---
+++ Python Assignment 1.py
@@ -0,0 +1,25 @@
+Python 3.13.5 (tags/v3.13.5:6cb20a2, Jun 11 2025, 16:15:46) [MSC v.1943 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> a=5
+>>> b=10
+>>> c=a+b
+>>> d=a-b
+>>> e=a*b
+>>> f=a/b
+>>> 
+>>> print(c)
+15
+>>> print(d)
+-5
+>>> print(e)
+50
+>>> print(f)
+0.5
+>>> 
+>>> 
+>>> e="Ravi"
+>>> f="Pandit"
+>>> g="Hello, "
+>>> h="! Welocome to the Python program"
+>>> print(g+e+f+h)
+Hello, RaviPandit! Welocome to the Python program
